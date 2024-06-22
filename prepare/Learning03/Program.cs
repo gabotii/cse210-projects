844c5398b579b92ba7127231239fd949c6d395a8
gabotii <gtorrejonaguilera@gmail.com>
2024-06-22 14:56:48 -0400

    Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args)    
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetFractionDecimal());
+        Fraction frac2 = new Fraction(5);
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetFractionDecimal());
+        Fraction frac3 = new Fraction(3,4);
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetFractionDecimal());
     }
 }
