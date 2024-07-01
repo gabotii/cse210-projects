cc71b67fc50ab365523277ef5e9fb574bb3a9204
gabotii <gtorrejonaguilera@gmail.com>
2024-07-01 12:07:35 -0400

    AddedW04

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,16 @@
+public class Assignment
+{
+    protected string _studentName; // Changed to protected
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
